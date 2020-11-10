292202f91538f146545d11f087ebdeab215a6422
Leo Gao <54557097+leogao2@users.noreply.github.com>
2020-11-10 15:01:47 -0700

    Segment by week

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ for date, chan, content in pool.imap(get_logs_for, documents()):
     if len(exclude_system(content)) > 0:
         content = clean(exclude_select_system(content))
         #print(date, chan)
-        logs[(chan, date.month)] += f'#{chan} {date.year}-{date.month:02d}-{date.day:02d}\n'
-        logs[(chan, date.month)] += content
+        logs[(chan, date.isocalendar()[1])] += f'#{chan} {date.year}-{date.month:02d}-{date.day:02d}\n'
+        logs[(chan, date.isocalendar()[1])] += content
         #if chan == 'ubuntu': print(content)
 
 ar = lmd.Archive('out')
